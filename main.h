4cafd68112dee7ec06b174406ded6c518b7f4cc4
MAYM7 <mmkoko8@gmail.com>
2023-10-13 14:37:16 -0700

    changing Header

--- main.h
+++ main.h
@@ -1,12 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
-int _print_char(va_list args);
-int _print_int(va_list args);
-int _print_str(va_list args);
-int (*_selec_func(char c))(va_list);
-int _putchar(char c);
 
 #endif
